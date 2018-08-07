9ad793dd64fd589ab59d8383e043bcd0533ab500
anmoldesai <anmol.desai@lendi.com.au>
2018-08-08 08:36:21 +1000

    basic static page

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Samle site for crash course.</p>
+  </div>
+)
+
+export default IndexPage
---
+++ src/pages/services.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ServicesPage = () => (
+  <div>
+    <h1>Services Page</h1>
+    <p>
+      lorem ipsum
+    </p>
+  </div>
+)
+
+export default ServicesPage
